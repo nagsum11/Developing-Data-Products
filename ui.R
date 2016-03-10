fd8f4a4a539a6211bfa8c252e285e4942c75e648
nagsum11 <nagsum11@yahoo.com>
2016-03-10 11:00:49 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -2,21 +2,25 @@ library(shiny)
 
  shinyUI( pageWithSidebar( 
    # Application title 
-         headerPanel("Addition of two Numbers!"), 
+         headerPanel("Mean/Median/Sum Calculator Application!"), 
             
            sidebarPanel( 
-                     textInput('guess1', 'Number1', value = ""),
-                     textInput('guess2', 'Number2', value = ""),
-                     actionButton("goButton", "Go!") 
+             h3("Instructions"),
+             p("This application Tells Mean/Median/Sum over three input values..."),
+                     numericInput('Input1', 'Number1',0, min = 1, max = 20, step = 1),
+                     numericInput('Input2', 'Number2', 0, min = 1, max = 20, step = 1),
+                     numericInput('Input3', 'Number3', 0, min = 1, max = 20, step = 1),
+             selectInput("operation", label = h3("Operation"), choices = list("SUM" = 1, 
+                          "MEAN" = 2, "MEDIAN" = 3, selected = 1))        
              ),  
            mainPanel( 
                      
-             h2('Input any two integers in given text boxes and hit Go button!'), 
-                              h5('This application add any two given integers and shows the result back'), 
+             h2('Input any 3 integers in given text boxes and select Operation!'), 
+             h5('This application Tells Mean/Median/Sum on three input values.'), 
              
-             h3('Results of Addition is:-'),
+             h3('Result is:-'),
                      
-                     verbatimTextOutput("outputValue") 
+                     verbatimTextOutput("results") 
              ) 
    ) 
    ) 
